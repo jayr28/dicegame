9d9c7ec8941f29ecb41f55c6e46cbc7397311fc6
jayr28 <poserio.rodolfo@gmail.com>
2018-09-14 00:24:55 -0500

    updated dicegame

--- games.js
+++ games.js
@@ -2,79 +2,190 @@
 
 //create variables
 
-let fourSidedDie =[6,8,10,12];
-let sixSidedDie = ['a','e','i','o','u','free'];
-let eightSidedDie = ['g','a','m','e','o','v','e','r'];
-let twentySidedDie = ['5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25'];
-let words = ["javascript","devcodecamp","programming","syntax","sublimetext","slack","stackoverflow","w3schools","phyton","ruby","php","yttrium","visualstudio","java","HTML","CSS"];
+let fourSidedDie =["top nfl teams","sports","top nfl quarterback(last name)","top nba players(last name)"];
+let sixSidedDie = ['eagles','packers','steelers','vikings','patriots','falcons'];
+let eightSidedDie = ['bastketball','football','soccer','boxing','tennis','golf','boxing','volleyball'];
+let tenSidedDie = ['rodgers','brady','newton','brees','rivers','newton','wilson','ryan','stafford','garopollo']
+let twelveSidedDie = ['antetokounmpo','curry','james','harden','durant','westbrook','thompson','davis','lillard','mitchell','george','leonard','irving']
+let twentySidedDie = ['1','2','3','4','5','6','7','8','9','10'];
 
-function randomWord(){
 
 
-		let guessWord = Math.floor(Math.random()*words.length);
 
-		return guessWord;
-		
+runGuessWord();
+
+function runGuessWord() {
+
+	alert("are you ready to guess the words?")
+	//let answer = rollDie();
+	let numberOfGuesses = (twentySidedDie[rollDie(10)]);
+
+	let playerGuessWord = playerGuess(numberOfGuesses);
+
 }
 
-let wordIndex = randomWord();
+function playerGuess(numberOfGuesses){
 
-console.log(words[wordIndex]);
+	// let answer = console.log(roll);
+	let guessCounter = 0;
+	let tryToGuess = "";
+	let play = getAnswer();
+	let tryAgain = true;
+	tryToGuess = prompt("what is your guess?");
+	while ( tryAgain ){
 
+
+
+
+			if (play == tryToGuess ){
+					
+					alert("you guessed correctly!");
+			
+					tryAgain = false;
+		
+			} else {
+
+					guessCounter ++;
+
+	        		if ( numberOfGuesses == guessCounter){
+
+					alert ("YOUVE REACH THE MAXIMUM OF GUESSES, BETTER LUCK NEXT TIME DUDE! HAVE DRINK AND TRY AGAIN!");
+
+					tryAgain = false;
+			
+			}	else {
+
+					
+
+					tryToGuess = prompt("try again, you only have " + (numberOfGuesses-guessCounter) );
+					
+					tryAgain = true;
+				}
+
+					
+
+
+					
+			}
+
+		}
+
+
+
+	// 	if ( numberOfGuesses == guessCounter){
+
+	// 			alert ("YOUVE REACH THE MAXIMUM OF GUESSES, BETTER LUCK NEXT TIME DUDE! HAVE DRINK AND TRY AGAIN!");
+
+	// 			tryAgain = false;
+
+	// 		} else {
+
+	// 				if (play == tryToGuess ){
+					
+	// 				alert("you guessed correctly!");
+			
+	// 				tryAgain = false;
+		
+	// 		} else {
+	// 				guessCounter ++;
+
+	// 				tryToGuess = prompt("try again, you only have " + (numberOfGuesses-guessCounter) );
+
+	// 				tryAgain = true;
+
+
+					
+	// 		}
+			
+
+	// 	}
+
+			
+
+		
+	// }
+		
+}
 
 
 function rollDie(numOfSides) {
 
 	let roll = Math.floor(Math.random()*numOfSides)+1;
  
-	return roll;
+		return roll;
 
 }
+
+
+
+function getAnswer(){
 
 let rollIndex = rollDie(4);
+alert(fourSidedDie[rollIndex-1]);
 
-console.log(fourSidedDie[rollIndex-1]);
+let index = rollIndex - 1;
+//let category = (fourSidedDie[rollIndex-1]);
 
-	if (rollIndex == 6) {
+
+//do the mapping here of the category to the generated answers
+//
+
+	if (index == 0){ // NFL teams 
 		let rollIndexTwo = rollDie(6);
-		console.log(sixSidedDie[rollIndexTwo]);
-	
-	} else if (rollIndex == 8) {
-		let rollIndexThree = rollDie(8);
+		
+		//console.log(sixSidedDie[rollIndexTwo-1]);
+		return sixSidedDie[rollIndexTwo-1];
+		//return rollIndexTwo;
 
-		console.log(eightSidedDie[rollIndexThree]);
+		} else if (index == 1){ //sports
 
-	}
- //create a random number for 4 sided dice with 6 8 10 12
+			let rollIndexThree = rollDie(8);
+			// return rollIndexThree;
+			return  eightSidedDie[rollIndexThree-1];
+
+		} else if (index == 2){ //nfl quarterbacks
+
+			let rollIndexfour = rollDie(10);
+			return  tenSidedDie[rollIndexfour-1];
+			
 
 
-//create a random selection for a,e,i,o,u free
+		} else if (index == 3){ //nba players
 
-//create 8 questions for 10 categories
+			let rollIndexfive = rollDie(12);
+			return  twelveSidedDie[rollIndexfive-1];
+			
+		}
 
-// create 10 categories
-
-//create 12 random selection for life
-
-let rollIndexSix = rollDie(20);
-function countdown(rollIndexSix) {
-    //initialize the variable to be returned with the initial i
-    var ret = rollIndexSix;
-    //in each iteration, assigns i to i-1 then checks if i >= 0
-    while (--rollIndexSix >= 0) {
-        //concatenates a space and the current i value to the return string
-        ret += ' ' + rollIndexSix;
-    }
-    //returns the string
-    return ret;
+		return category;
 }
-console.log(twentySidedDie[rollIndexSix]);
-//create a countdown from 20 seconds
 
-function getUserInput(guess){
-	let userInput = prompt(guess);
-	return userInput;
+
+
+function guessWord(answer,guess){
+
+	var answer = "";
+	var guess = prompt("what is your guess?")
+		if (answer == guess ){
+			alert("you guessed correctly!");
+			break;
+		} else {
+			guess = prompt("try again");
+		}
 }
+
+function displayResult(result, message) {
+    console.log(message + result);
+   }
+
+// console.log(myVar);
+
+
+
+
+
+
+
 
 
 
